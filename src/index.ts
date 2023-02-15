e958934a7944a71426173648710c6f2703d16eaa
Dionei Silva <dionei.silva@gmail.com>
2023-02-15 17:28:17 -0300

    Realizado balance

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,39 @@ function addTransaction(wallet: Wallet, transaction: Transaction) {
   wallet.transactions.push(transaction);
 }
 
-function printTransactions(wallet:Wallet){
+function printTransactions(wallet: Wallet) {
   // const transactions = wallet.transactions;
-  const {transactions,owner} = wallet
+  const { transactions, owner } = wallet;
 
   console.log(`Está carteira é do (a) ${owner.name}\n `);
-  
-  transactions.forEach((item)=> console.log(`Transação:${item.description}\n Valor: R$${item.value}\n Tipo:${item.type}\n`));
-  
-  
-  
+
+  transactions.forEach((item) =>
+    console.log(
+      `Transação:${item.description}\n Valor: R$${item.value}\n Tipo:${item.type}\n`
+    )
+  );
+}
+
+function balance(wallet: Wallet): number {
+  const { transactions } = wallet;
+  let saldo = 0;
+
+  transactions.forEach((item) => {
+    if (item.type === "C") {
+      saldo += item.value;
+    } else {
+      saldo -= item.value;
+    }
+  });
+
+  return saldo;
 }
 
 addTransaction(walletUser, { description: "mercado2", type: "D", value: 200 });
 addTransaction(walletUser, { description: "shopping", type: "D", value: 2000 });
 addTransaction(walletUser, { description: "salario", type: "C", value: 20000 });
 
-printTransactions(walletUser)
+/* printTransactions(walletUser); */
+
+const saldo = balance(walletUser);
+console.log(`Seu saldo é R$:${saldo}.`);
